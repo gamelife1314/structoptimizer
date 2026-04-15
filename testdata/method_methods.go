84383196f18b9cb2a552855ccb3fa593dd0f0391
michael <1185694600@qq.com>
2026-04-15 14:16:14 +0800

    test: 添加指针和值接收者方法测试案例

--- testdata/method_methods.go
+++ testdata/method_methods.go
@@ -1,11 +1,36 @@
 package testdata
 
-// Marshal 是 MethodStruct 的方法（在另一个文件中）
+// === 指针接收者方法 ===
+
+// PtrMethod 是指针接收者方法
+func (p *PtrMethodStruct) PtrMethod() error {
+	return nil
+}
+
+// PtrMethod2 是另一个指针接收者方法
+func (p *PtrMethodStruct) PtrMethod2() ([]byte, error) {
+	return nil, nil
+}
+
+// === 值接收者方法 ===
+
+// ValueMethod 是值接收者方法
+func (v ValueMethodStruct) ValueMethod() {
+}
+
+// ValueMethod2 是另一个值接收者方法
+func (v ValueMethodStruct) ValueMethod2() int {
+	return 0
+}
+
+// === 混合接收者方法 ===
+
+// MethodStruct 的指针接收者方法（跨文件定义）
 func (m *MethodStruct) Marshal() ([]byte, error) {
 	return nil, nil
 }
 
-// Unmarshal 是 MethodStruct 的方法
-func (m *MethodStruct) Unmarshal(data []byte) error {
+// MethodStruct 的值接收者方法
+func (m MethodStruct) Unmarshal(data []byte) error {
 	return nil
 }
--- testdata/method_struct.go
+++ testdata/method_struct.go
@@ -5,3 +5,16 @@ type MethodStruct struct {
 	Name string
 	ID   int64
 }
+
+// PtrMethodStruct 用于测试指针接收者的结构体
+type PtrMethodStruct struct {
+	A int8
+	B int64
+	C int8
+}
+
+// ValueMethodStruct 用于测试值接收者的结构体
+type ValueMethodStruct struct {
+	X int64
+	Y int32
+}
